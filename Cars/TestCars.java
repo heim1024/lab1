cd7e1e4a7d719f8abd32cec799a7cca46109af22
heim1024 <melwin.heimby@gmail.com>
2025-02-05 09:57:04 +0100

    meckad again

---
+++ Cars/TestCars.java
@@ -0,0 +1,14 @@
+package Cars;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestCars {
+
+    @Test
+    public void exampleTest() {
+        assertTrue(true);
+    }
+
+}
